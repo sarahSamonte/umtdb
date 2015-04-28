4892c2d55cfe2eaa027934c5887d13da64b47342
JolGspr <jolinagaspar@yahoo.com>
2015-04-29 06:21:14 +0800

    fixed index front end and deleted demo.css from header_view

--- application/views/inc/header_view.php
+++ application/views/inc/header_view.php
@@ -7,7 +7,6 @@
 <link rel="stylesheet" href="<?=base_url()?>public/css/flat-ui.css"	/>
 <link rel="stylesheet" href="<?=base_url()?>public/css/bootstrap.min.css"	/>
 <link rel="stylesheet" href="<?=base_url()?>public/css/flat-ui.min.css"	/>
-<link rel="stylesheet" href="<?=base_url()?>public/css/demo.css"	/>
 <link rel="stylesheet" href="<?=base_url()?>public/css/style.css"	/>
 
 <link rel="shortcut icon" href="<?=base_url()?>public/img/logoimg.ico"	/>
