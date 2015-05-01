478052ef887f781d11392b4c6aae338ac0a1e7b6
JolGspr <jolinagaspar@yahoo.com>
2015-05-02 01:34:11 +0800

    Revert "fixed index front end and deleted demo.css from header_view"
    This reverts commit 4892c2d55cfe2eaa027934c5887d13da64b47342.

--- application/views/index_view.php
+++ application/views/index_view.php
@@ -27,41 +27,43 @@ html, body{
 
 <body>
 
-<div id="index" class="container" style="width:970px !important;">
-<div class="row" style="padding-top: 90px;">
+
+<div id="index" class="container">
+
+<div class="row demo-samples">
 <!-- WELCOME DIV -->
 <div class="col-xs-8">
-  <div class="parWrap">
-    <h2>Welcome!</h2>
-    <div class="login-icon">
-      <img src="<?=base_url()?>public/img/logo.png" width="89%" height="150"/>
-    </div>
-    <h6><br /><br /><br /><br /><br /><br />Electricity and Water Consumption Database of the University of the Philippines Diliman.</h6>
-  </div>  <!--parWrap -->
+	<div class="parWrap">
+		<h2>Welcome!</h2>
+		<div class="login-icon">
+			<img src="<?=base_url()?>public/img/logo.png" width="89%" height="150"/>
+		</div>
+		<h6><br /><br /><br /><br /><br /><br />Electricity and Water Consumption Database of the University of the Philippines Diliman.</h6>
+	</div>  <!--parWrap -->
 </div><!-- col-xs-8 -->
 <!-- WELCOME DIV -->
+
 
 <!-- LOG-IN DIV -->
 <div class="col-xs-4">
 <div id="logincon">
 <div class="login-form"><h3>LOG-IN<br></h3>
 <?php echo form_open('index/login'); ?>
-<form id="loginForm" name="loginForm" method="POST" action="">
-  <div class="form-group">
-      <input required="required" type="text" class="form-control login-field" value="" placeholder="Username" name="username" id="username" />
-      <label class="login-field-icon fui-user" for="username"></label>
+	<div class="form-group">
+    	<input required="required" type="text" class="form-control login-field" value="" placeholder="Username" name="username" id="username" />
+    	<label class="login-field-icon fui-user" for="username"></label>
     </div> <!-- form-group -->
     
     <div class="form-group">
-      <input required="required" type="password" class="form-control login-field" value="" placeholder="Password" name="password" id="password" />
-      <label class="login-field-icon fui-lock" for="password"></label>
+    	<input required="required" type="password" class="form-control login-field" value="" placeholder="Password" name="password" id="password" />
+    	<label class="login-field-icon fui-lock" for="password"></label>
     </div> <!-- form-group -->
     
     <input required="required" type="submit" class="btn btn-info btn-lg btn-block" value="Log in" name="login" id="login" />
 
 </form>
 
-<a class="login-link" href="#forgotPasswordModal" data-toggle="modal">Forgot your password?</a> 
+<a class="login-link" href="#forgotPasswordModal" data-toggle="modal">Forgot your password?</a>	
 </div> <!-- login-form -->
 </div> <!-- loginicon -->
 </div>  <!-- .col-xs-4 -->
@@ -73,32 +75,30 @@ html, body{
 <div class="modal fade bs-example-modal-sm" id="forgotPasswordModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 <div class="modal-dialog modal-sm">
 <div class="modal-content">
-  <div class="modal-header modal-header-info">
-    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    <h4 class="modal-title">Forgot Password?</h4>
-  </div> <!-- modal-header -->
-  
-  <div class="modal-body">
-  <p>Email Address:</p>
-  <form id="forgotPassForm" name="forgotPassForm" method="post" action="">
-  
-    <div class="form-group">
-    <div class="col-md-12 input-group">
-      <span class="input-group-addon">@</span>
-      <input required="required" type="email" class="form-control" id="email" name="email" placeholder="Email Address"/>
-    </div> <!-- end of col-md-->       
-    </div> <!-- form-group -->    
-  </div><!-- modal-body -->
-  
-  <div class="modal-footer">
-    <input required="required" type="submit" class="btn btn-info pull-right" value="Send to email" name="fPSubmit" id="fPSubmit" />                                      
-  </div><!-- modal-footer -->
-  </form>
+	<div class="modal-header modal-header-info">
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		<h4 class="modal-title">Forgot Password?</h4>
+	</div> <!-- modal-header -->
+	
+	<form id="forgotPassForm" name="forgotPassForm" method="post" action="">
+	<div class="modal-body">
+		<p>Email Address:</p>
+		<div class="form-group">
+		<div class="col-md-12 input-group">
+			<span class="input-group-addon">@</span>
+			<input required="required" type="email" class="form-control" id="email" name="email" placeholder="Email Address"/>					
+		</div> <!-- form-group -->		
+	</div><!-- modal-body -->
+	
+	<div class="modal-footer">
+		<input required="required" type="submit" class="btn btn-info pull-right" value="Send to email" name="fPSubmit" id="fPSubmit" />	                                     
+	</div><!-- modal-footer -->
+	</form>
 
-</div><!-- modal-Content -->
+</div><!-- modal-ontent -->
 </div><!-- modal-dialog -->
 </div><!-- modal -->
 <!-- FORGOT PASSWORD MODAL -->
- 
+
+</div><!-- container -->
 <div class = "push"></div>
-</div> <!-- end of container div -->
